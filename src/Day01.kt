d33eda51dd199cba6da102a161807f1c2450fb34
danwal <daniel.wallman@avanza.se>
2021-12-25 19:27:17 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increased = 0
+        val inputNums = input.map { Integer.parseInt(it) }
+        for (i in 0..inputNums.size-2) {
+            if (inputNums[i + 1] > inputNums[i]) {
+                increased += 1
+            }
+        }
+        return increased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increased = 0
+        val inputNums = input.map { Integer.parseInt(it) }
+        for (i in 0..inputNums.size-4) {
+            if (inputNums[i + 3] > inputNums[i]) {
+                increased += 1
+            }
+        }
+        return increased
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_part2_test")
+    println("Test: " + part2(testInput))
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
